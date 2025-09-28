57f7d6a56734ccd29831f040a640b93cab835f66
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-09-28 14:45:50 +0600

    basic-routing-and-nested-routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1 className="text-4xl">This is About Page </h1>
+        </div>
+    );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContactPage() {
+    return (
+        <div>
+            <h1 className="text-4xl">This is Contact Page </h1>
+        </div>
+    );
+}
